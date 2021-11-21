c8231a9fe0df180e0fae9ab7575d2c93fd756842
Vageshwar <vageshwaryadav@gmail.com>
2021-11-21 21:35:44 +0530

    database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { AuthModule } from './modules/auth.module';
     host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'root',
+    password: '',
     database: 'test',
     entities: [],
     synchronize: false,
